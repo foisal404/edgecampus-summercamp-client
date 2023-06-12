6f5d1b0ca6bf1eb7836cbd1d4150d0546262a064
foisal404 <foisalahmmed2@gmail.com>
2023-06-13 01:50:18 +0600

    theme toogle change in navbar

--- src/pages/Shared/NavHeadBar.jsx
+++ src/pages/Shared/NavHeadBar.jsx
@@ -1,6 +1,32 @@
 import {  FaMoon } from 'react-icons/fa';
+import { BsSun } from "react-icons/bs";
 import navImg from '../../../public/campii.png'
+import { useEffect, useState } from 'react';
+
 const NavHeadBar = () => {
+    //theme set 
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+      );
+    
+      // update state on toggle
+      const handleToggle = (e) => {
+        if (e.target.checked) {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      };
+    
+      // set theme state in localstorage on mount & also update localstorage on state change
+      useEffect(() => {
+        localStorage.setItem("theme", theme);
+        const localTheme = localStorage.getItem("theme");
+        // add custom data-theme attribute to html tag required to update theme using DaisyUI
+        document.querySelector("html").setAttribute("data-theme", localTheme);
+      }, [theme]);
+
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -72,7 +98,17 @@ const NavHeadBar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn"><FaMoon/></a>
+        <button className="btn btn-square btn-ghost">
+          <label className="swap swap-rotate w-12 h-12">
+            <input type="checkbox"
+                onChange={handleToggle}
+                // show toggle image based on localstorage theme
+                checked={theme === "light" ? false : true}
+            />
+            <BsSun className="w-8 h-8 swap-on" />
+            <FaMoon alt="dark" className="w-8 h-8 swap-off" />
+          </label>
+        </button>
       </div>
     </div>
   );
