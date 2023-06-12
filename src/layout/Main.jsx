deb3722fdaa53a9b8542f1b5738fa013bc9b110a
foisal404 <foisalahmmed2@gmail.com>
2023-06-13 01:16:33 +0600

    basic setup of react router ,taidwind ,icon,daisyui ,sweetalart

---
+++ src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import NavHeadBar from "../pages/Shared/NavHeadBar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavHeadBar/>
+        </div>
+    );
+};
+
+export default Main;
