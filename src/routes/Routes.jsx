83eb80c0c901c228d73126714b92733bfcff2ff7
foisal404 <foisalahmmed2@gmail.com>
2023-06-15 02:13:51 +0600

    errorpage add

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
+import Errorpage from "../pages/errorpage/Errorpage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main/>,
+      errorElement:<Errorpage/>,
       children:[
         {
             path:"/",
